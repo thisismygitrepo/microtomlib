4a8ad8e4ecd541e4d8941d5f3de556eb52cfbd15
lei guo <uqlguo3@leis-MacBook-Pro.local>
2020-08-05 12:51:25 +0800

    I revised these two functions ("inner_product" and "square_norm_sum") for MR-CSI solver

--- solvers/MR_CSI_solver/inner_product.m
+++ solvers/MR_CSI_solver/inner_product.m
@@ -1,4 +1,4 @@
-function [inner_production] = inner_product(X, Y)
+function [inner_production] = inner_product(X, Y, dx, dy)
 
     if size(X, 1) ~= size(Y, 1) || size(X, 2) ~= size(Y, 2)
         error('The size of X and Y have to be the same\n')
@@ -9,7 +9,7 @@ function [inner_production] = inner_product(X, Y)
     inner_production = zeros(column_num, 1);
        
     for k = 1 : column_num
-        inner_production(k) = (X(:, k).' * conj(Y(:, k)));
+        inner_production(k) = dx * dy * (X(:, k).' * conj(Y(:, k)));
     end
 end
           
