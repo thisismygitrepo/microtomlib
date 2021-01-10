bf844f3fac2df3b477c0d3c238aaa817c159f0f7
Alex <programmer@usa.com>
2021-01-11 08:12:20 +1000

    BP solver is ready

--- bp_solver/test_function.m
+++ bp_solver/test_function.m
@@ -1,3 +1,5 @@
+
+
 %%% This code is only used for testing and debug the new BP solver
 
 clc
@@ -5,10 +7,15 @@ clear
 
 pm = path_manager();
 
+
 tmp = "\emvision\Algorithm\toml_data\simulations\wiener\large_stroke_simulations\example1\wIXI087-Guys-0768-T1_t0_1.s16p";
 target_filename = pm.join(pm.gdrive, tmp);
+
+
 Cal1_filename = pm.join(pm.gdrive, "emvision\Algorithm\toml_data\simulations\wiener\cals\Cal1.s16p");
 Cal2_filename = pm.join(pm.gdrive, "/emvision/Algorithm/toml_data\simulations\wiener\cals\Cal2.s16p");
 
+
 [eps_BP, sigma_BP] = func_BP_CST_dynamic(730.4e6, 4, target_filename, Cal1_filename, Cal2_filename);   
-save("res_dir/results.m")
+save("results/results.mat", "eps_BP", "sigma_BP")
+
